c8ac348dfcf48564dfd1a0184f46f1f7cb965b9e
Rianone <84690507+Rianone@users.noreply.github.com>
2023-03-27 23:57:46 +0100

    New commit

--- screens/Home.js
+++ screens/Home.js
@@ -4,6 +4,7 @@ import { useNavigation } from "@react-navigation/native";
 import { FontAwesome } from '@expo/vector-icons';
 import colors from '../colors';
 import { Entypo } from '@expo/vector-icons';
+import { ImageBackground } from "react-native";
 const catImageUrl = "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=49ed3252c0b2ffb49cf8b508892e452d";
 
 const Home = () => {
@@ -37,12 +38,14 @@ const Home = () => {
 
     return (
         <View style={styles.container}>
+            {/* <ImageBackground source={require("../assets/images/bg.jpg")} resizeMode="cover" style={styles.image}> */}
             <TouchableOpacity
                 onPress={() => navigation.navigate("Chat")}
                 style={styles.chatButton}
             >
                 <Entypo name="chat" size={24} color={colors.lightGray} />
-            </TouchableOpacity>
+                </TouchableOpacity>
+            {/* </ImageBackground> */}
         </View>
     );
 };
@@ -72,5 +75,10 @@ const styles = StyleSheet.create({
         shadowRadius: 8,
         marginRight: 20,
         marginBottom: 50,
+    },
+    image: {
+        // width: "100%",
+        // height: "50%"
+        // backgroundSize:"cover"
     }
 });
